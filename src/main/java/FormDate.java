d5d8b63286f71a736d0d7a9aeef08b9e8baf455e
Elnar Kilmukhametov <soulmiker@gmail.com>
2022-07-27 18:21:25 +0300

    Initial Commit

---
+++ src/main/java/FormDate.java
@@ -0,0 +1,5 @@
+public class FormDate {
+    public int day; // поле для дня рождения;
+    public int month; // месяц рождения
+    public int year; // год рождения
+}
---
+++ src/main/java/Post.java
@@ -0,0 +1,9 @@
+public class Post {
+    public String name; // поле для имени
+    public String surname; // поле для фамилии
+    public String patronymic; // поле для отчества
+    public String phone; // поле для номера телефона
+    public String passport; // поле для номера паспорта
+    public boolean subscription; // поле для подписки
+    public FormDate birthday;
+}
